3a6e94a56e42bcdd259f87308e06e11a8f122462
ibnucholdun <ibnucholdun023@gmail.com>
2025-11-24 12:47:38 +0700

    feat: addd card list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Hero from "@/components/Hero";
+import Main from "@/components/Main";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold uppercase">Rooms & Rates</h1>
+          <p className="py-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, quia!
+          </p>
+        </div>
+        <Main />
+      </div>
     </div>
   );
 };
